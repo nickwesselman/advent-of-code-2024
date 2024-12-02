2a439f5ae6346fe539fa3ab3452513b3e73af6dd
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2024-12-02 17:35:08 -0500

    Day 2 part 1 complete

---
+++ day2.p1.rb
@@ -0,0 +1,51 @@
+def is_safe (report)
+  if !is_increasing(report) && !is_decreasing(report)
+    return false
+  end
+  
+  report.each.with_index do |level, index|
+    if index == 0
+      next
+    end
+
+    from_previous = (level - report[index-1]).abs
+    if from_previous < 1 || from_previous > 3
+      return false
+    end
+  end
+
+  return true
+end
+
+def is_increasing (report)
+  report.each.with_index do |level, index|
+    if index == 0
+      next
+    elsif level < report[index-1]
+      return false
+    end
+  end
+  return true
+end
+
+def is_decreasing (report)
+  report.each.with_index do |level, index|
+    if index == 0
+      next
+    elsif level > report[index-1]
+      return false
+    end
+  end
+  return true
+end
+
+
+safe_reports = 0
+File.open("day2.input.txt").read.each_line { |report_line|
+  report = report_line.split(/\s/).map { |level| level.to_i }
+  if is_safe(report)
+    safe_reports += 1
+  end
+}
+
+puts safe_reports
