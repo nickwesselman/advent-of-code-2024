114588fb4c34ad5867dd1bda1ba8837b6d927c4a
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2024-12-10 09:19:12 -0500

    Day 10 part 2!

--- day10.rb
+++ day10.rb
@@ -1,18 +1,17 @@
 require "test/unit"
 require "set"
 
+
 def try_path(map, x, y, current_height, found_nines)
   if x < 0 || y < 0 || x >= map[0].length || y >= map.length
-    return
+    return 0
   elsif map[y][x] != current_height + 1
-    return
+    return 0
   elsif map[y][x] == 9
-    #puts map[y][x]
-    #puts "FOUND ONE"
     found_nines << [x,y]
+    return 1
   else
-    #puts map[y][x]
-    [[x-1,y],[x,y-1],[x+1,y],[x,y+1]].each { |node| try_path(map, node[0], node[1], current_height+1, found_nines)}
+    return [[x-1,y],[x,y-1],[x+1,y],[x,y+1]].reduce(0) { |sum,node| sum + try_path(map, node[0], node[1], current_height+1, found_nines)}
   end
 end
 
@@ -35,7 +34,20 @@ def part_one(input)
   end
 end
 
+def part_two(input)
+  map = Array.new
+  input.readlines.each do |line|
+    map << line.strip.chars.map { |x| x.to_i }.to_a
+  end
 
+  _unused = Set.new
+  return map.each.with_index.reduce(0) do |sum_y,(rows,y)|
+    row_total = rows.each.with_index.reduce(0) do |sum_x,(node,x)|
+      sum_x + (node == 0 ? try_path(map, x, y, -1, _unused) : 0)
+    end
+    sum_y + row_total
+  end
+end
 
 class Day10Test < Test::Unit::TestCase
 
@@ -51,16 +63,16 @@ class Day10Test < Test::Unit::TestCase
     assert(result > 0)
   end
 
-  # def test_part_two_example
-  #   result = part_two(File.open("day9.example.txt"))
-  #   puts "P2 EXAMPLE RESULT: #{result}"
-  #   assert_equal(2858, result)
-  # end
+  def test_part_two_example
+    result = part_two(File.open("day10.example.txt"))
+    puts "P2 EXAMPLE RESULT: #{result}"
+    assert_equal(81, result)
+  end
 
-  # def test_part_two
-  #   result = part_two(File.open("day9.input.txt"))
-  #   puts "P2 RESULT: #{result}"
-  #   assert(result > 0)
-  # end
+  def test_part_two
+    result = part_two(File.open("day10.input.txt"))
+    puts "P2 RESULT: #{result}"
+    assert(result > 0)
+  end
 
 end
